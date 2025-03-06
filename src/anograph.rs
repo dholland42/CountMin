a8394fccc74c5fc7959d042de98370f4b929e7c5
Daniel Holland <dholland414@gmail.com>
2025-03-06 04:03:44 +0000

    handle empty inputs and full row or column masks in density computation

--- src/anograph.rs
+++ src/anograph.rs
@@ -5,7 +5,13 @@ pub fn density <const R: usize, const C: usize> (sketch: &[[f64; C]; R], rmask: 
             sum += sketch[r][c];
         }
     }
-    sum / f64::sqrt(rmask.iter().filter(|v| **v).count() as f64 * cmask.iter().filter(|v| **v).count() as f64)
+    let denom = f64::sqrt(rmask.iter().filter(|v| **v).count() as f64 * cmask.iter().filter(|v| **v).count() as f64);
+    
+    // Make sure we handle the empty case.
+    match denom {
+        0. => 0.,
+        _ => sum / denom
+    }
 }
 
 
@@ -21,7 +27,15 @@ mod tests {
         ];
         let rmask = [true, false];
         let cmask = [true, true, true];
+        // Given the mask, we should get (0 + 1 + 2) / sqrt(3).
         assert_eq!(density(&items, &rmask, &cmask), 3. / f64::sqrt(3.));
-        
+        // If things are empty, we get 0.
+        assert_eq!(density(&[], &[], &[]), 0.);
+        // If everything is masked, we get 0.
+        assert_eq!(density(&items, &[false, false], &[false, false, false]), 0.);
+        // If all rows are masked, we get 0.
+        assert_eq!(density(&items, &[false, false], &[true, true, true]), 0.);
+        // If all columns are masked, we get 0.
+        assert_eq!(density(&items, &[true, true], &[false, false, false]), 0.);
     }
 }
